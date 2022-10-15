94ddc198e77da4e5782b37007618d94b8b4bcaa8
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-10-16 01:14:35 +0900

    css 추가 중

--- javascript.js
+++ javascript.js
@@ -1,36 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const inputBtn = document.querySelector('button')        
+    const inputBtn = document.querySelector('.contents_input--btn')        
     const contents = document.getElementById("contents")
     const contentsInput = document.getElementById("contents_input")
     const saveBtn = document.querySelector('.contents_save')    
- 
+    const url = window.location.href.split("/")[window.location.href.split("/").length-1]
+
+
+
+    console.log(localStorage.key(2))
     // index.html 목록 띄우는 함수
     const load = () => {                     
         for(let i = 0; i < localStorage.length; i++){        
-            contents.innerHTML += "<p class='?'>"+ `${localStorage.key(i)}`+"</p>"
+            contents.innerHTML += "<br><span class='contents_main'>"+ `${localStorage.key(i)}`+"</span>"
         }        
         open();              
     }    
 
     // 클릭해서 정보 띄우기
     const open = () => {
-        const clickP = document.querySelectorAll('.contents p')           
+        const clickP = document.querySelectorAll('.contents span')           
         clickP.forEach(p => {
-            p.onclick = function() {
-                contents.innerHTML =""
+            p.onclick = function() {                
+                contents.innerHTML = ""
                 const storage = localStorage[this.textContent].split(",")
-                console.log(storage)
+                
                 for(let i=0; i<storage.length; i+=2){
-                    contents.innerHTML += "<input type='text' value="+`${storage[i]}`+ "><input type='number' value="+`${storage[i+1]}`+">"
+                    contents.innerHTML += "<input class='a' type='text' value="+`${storage[i]}`+ "><input class='b' type='number' value="+`${storage[i+1]}`+"><br>"
                 }
+                contents.innerHTML +="<br><input class='home' type='button' value='목록' onclick="+"location.href='index.html'"+">"
             }
+            
         })
     }
 
     // 작성 칸 추가 함수
     const addInput = () => {
         const newP = document.createElement("p");
-        newP.innerHTML = "<input type='text'><input type='number'>";
+        newP.innerHTML = "<input type='text'> <input type='number'>";
         contentsInput.appendChild(newP);
     }
     
@@ -46,10 +52,9 @@ document.addEventListener('DOMContentLoaded', () => {
         localStorage.setItem(date, arryInput);
     }
 
-    // 목록에서 내용 확인 하는 함수
     
 
-    window.addEventListener('load', load)
+    if(url == "index.html") window.addEventListener('load', load)
     inputBtn.addEventListener('click', addInput)
     if(saveBtn!=null) saveBtn.addEventListener('click', save)
 })
