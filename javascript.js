cdf1fdc127c5ed779657d6773f35e652353eb246
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-10-17 13:44:49 +0900

    수정본

--- javascript.js
+++ javascript.js
@@ -5,13 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const saveBtn = document.querySelector('.contents_save')    
     const url = window.location.href.split("/")[window.location.href.split("/").length-1]
 
-
-
-    console.log(localStorage.key(2))
+     
     // index.html 목록 띄우는 함수
-    const load = () => {                     
-        for(let i = 0; i < localStorage.length; i++){        
-            contents.innerHTML += "<br><span class='contents_main'>"+ `${localStorage.key(i)}`+"</span>"
+    const load = () => {         
+        const localStorageKey = []
+    
+        for(let i=0; i<localStorage.length; i++){
+            localStorageKey.push(localStorage.key(i))
+        }
+        localStorageKey.sort()
+        for(let i of localStorageKey){        
+            contents.innerHTML += "<br><span class='contents_list'>"+ `${i}`+"</span>"
         }        
         open();              
     }    
@@ -21,13 +25,23 @@ document.addEventListener('DOMContentLoaded', () => {
         const clickP = document.querySelectorAll('.contents span')           
         clickP.forEach(p => {
             p.onclick = function() {                
-                contents.innerHTML = ""
+                contents.innerHTML = "<div class='contents_main'></div>"
+                const contentsDiv = contents.querySelector(".contents_main")
                 const storage = localStorage[this.textContent].split(",")
+                let money = 0
                 
+                const newDiv1 = document.createElement("div")
+                const newDiv2 = document.createElement("div")
                 for(let i=0; i<storage.length; i+=2){
-                    contents.innerHTML += "<input class='a' type='text' value="+`${storage[i]}`+ "><input class='b' type='number' value="+`${storage[i+1]}`+"><br>"
+                    newDiv1.innerHTML += "<p>"+`${storage[i]}`+"</p>"
                 }
-                contents.innerHTML +="<br><input class='home' type='button' value='목록' onclick="+"location.href='index.html'"+">"
+                contentsDiv.appendChild(newDiv1)
+                for(let i=0; i<storage.length; i+=2){
+                    newDiv2.innerHTML += "<p>"+`${storage[i+1]}`+"</p>"
+                    money += parseInt(`${storage[i+1]}`)
+                }
+                contentsDiv.appendChild(newDiv2)
+                contents.innerHTML +="<span>총지출 : "+`${money}`+"</span>"+"<br><button class='home' onclick="+"location.href='index.html'"+">목록</button>"
             }
             
         })
@@ -36,7 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // 작성 칸 추가 함수
     const addInput = () => {
         const newP = document.createElement("p");
-        newP.innerHTML = "<input type='text'> <input type='number'>";
+        newP.innerHTML = "<input type='text'> <input class='add_input--b' type='number'>";
         contentsInput.appendChild(newP);
     }
     
@@ -55,6 +69,6 @@ document.addEventListener('DOMContentLoaded', () => {
     
 
     if(url == "index.html") window.addEventListener('load', load)
-    inputBtn.addEventListener('click', addInput)
+    if(url == "write.html") inputBtn.addEventListener('click', addInput)
     if(saveBtn!=null) saveBtn.addEventListener('click', save)
 })
